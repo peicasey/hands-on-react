d3c3d046e3ac5096b6f0783103a92bf01b691891
Casey <91210043+peicasey@users.noreply.github.com>
2022-12-23 18:06:52 +0000

    Added gasic style to app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,22 @@
+import group_pic from '../public/images/group.svg';
+const name = "Planet Observers"
+
+var clickedButton = () => alert('Howdy!'); 
+
 function App() {
   return (
-    <hgroup>
-      <h1>Meet the StarGazers</h1>
-      <p>Members of an <b>intergalactic alliance</b> paving the way for peace and benevolence among all species. They are known for their enthusiasm for science, for their love of fun, and their dedication to education.</p>
-    </hgroup>
+    <div className="main page">
+      <article>
+        <hgroup>
+          <img src={group_pic} alt="the bois" />
+          <h1>Meet the <b style={{color:"Pink"}}>{name}</b></h1>
+          <p>Members of an <b>intergalactic alliance</b> paving the way for peace and benevolence among all species. They are known for their enthusiasm for science, for their love of fun, and their dedication to education.</p>
+          <button className="outline" onClick={clickedButton}> 
+            {name} click here! 
+          </button>
+        </hgroup>
+      </article>
+    </div>
   )
 }
 export default App
