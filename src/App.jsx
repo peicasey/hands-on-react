30d8b53854ac1c169f5be60fdd371efc420b54a1
Casey <91210043+peicasey@users.noreply.github.com>
2022-12-23 18:17:11 +0000

    Tested Classes vs. Hooks

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,31 @@
+import React, { Component } from 'react';
+
+// REACT WITH CLASSES
+// class Welcome extends React.Component {
+//   constructor(props) {
+//     super(props);
+//   }
+
+//   render() {
+//     return <h1> Welcome! Meet the { this.props.name } </h1>
+//   }
+// }
+// <Welcome name={name}/>
+
+// REACT WITH HOOKS
+const Welcome = (props) => {
+  return <h1> Welcome! Meet the { props.name } </h1>
+}
+
+
 function App() {
-  const name = 'StarGazers'
+  const name = 'StarHomies'
   return (
     <div className="container">
       <article>
         <hgroup>
           <img src="images/group.svg" alt="StarGazers Group" />
-          <h1>Meet the <i style={{ color: "SteelBlue" }}>{name}</i></h1>
+          <Welcome name={name}/>
           <p>Members of an <b>intergalactic alliance</b><br />
             paving the way for peace and benevolence among all species. They are known for their enthusiasm for science, for their love of fun, and their dedication to education.</p>
           <button className="outline" onClick={() => alert('Hi there')}>Click Me</button>
