424380c8d7b1424808de7a06f654cb665549a1aa
Casey <91210043+peicasey@users.noreply.github.com>
2022-12-23 19:29:33 +0000

    added a function

--- src/components/Support.jsx
+++ src/components/Support.jsx
@@ -2,9 +2,10 @@ import { useState } from 'react';
 
 export default () => {
   const [count, setCount] = useState(0)
+  let increment = () => { setCount(count+1); }
 
   return (
-    <button className="outline" onClick={() => {setCount(count+1)}}>  
+    <button className="outline" onClick={increment}>  
       {count === 0 ? "Click to support" : `Supoort ${count} times!`}
     </button>
   )
